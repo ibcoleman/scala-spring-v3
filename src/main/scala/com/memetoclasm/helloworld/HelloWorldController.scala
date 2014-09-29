780e587e68c0a7a384976f8f38e4e5a711405ebf
icoleman <6jCKJ%rra6dF70xc>
2014-09-28 21:42:35 -0400

    Added html5 boilerplate resources. Updated web.xml configuration...

--- src/main/scala/com/memetoclasm/helloworld/HelloWorldController.scala
+++ src/main/scala/com/memetoclasm/helloworld/HelloWorldController.scala
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.{RequestMethod, RequestMapping}
 @Controller
 @RequestMapping(Array("/"))
 class HelloWorldController @Autowired() (nameService: Name) {
+
   @RequestMapping(method = Array(RequestMethod.GET))
   def index(model: Model) = {
     model.addAttribute("name", nameService.name)
