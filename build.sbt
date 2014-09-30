42826e945aabe688707dbfac2388c83834496da7
icoleman <6jCKJ%rra6dF70xc>
2014-09-29 22:35:00 -0400

    Added static resource path to Config.scala configuration class. Changed resource references in index.jsp to c:url tags.

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ seq(webSettings : _*)
 
 libraryDependencies ++= Seq(
   "org.springframework" % "spring-webmvc" % "4.0.0.RELEASE",
+  "jstl" % "jstl" % "1.2",
   "org.eclipse.jetty" % "jetty-webapp" % "9.1.0.v20131115" % "container, compile",
   "org.eclipse.jetty" % "jetty-jsp" % "9.1.0.v20131115" % "container"
 )
--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -1,10 +1,13 @@
 package com.memetoclasm.helloworld
 
-import org.springframework.context.annotation.{Bean, ComponentScan}
+import org.springframework.context.annotation.{Configuration, Bean, ComponentScan}
+import org.springframework.web.servlet.config.annotation.{EnableWebMvc, DefaultServletHandlerConfigurer, ResourceHandlerRegistry, WebMvcConfigurerAdapter}
 import org.springframework.web.servlet.view.{InternalResourceViewResolver, JstlView}
 
+@EnableWebMvc
+@Configuration
 @ComponentScan(basePackages = Array("com.memetoclasm.helloworld"))
-class Config {
+class Config extends WebMvcConfigurerAdapter {
 
   @Bean
   def viewResolver = {
@@ -13,6 +16,16 @@ class Config {
     viewResolver.setPrefix("/WEB-INF/views/")
     viewResolver.setSuffix(".jsp")
     viewResolver
+  }
+
+  @Override
+  override def addResourceHandlers( registry : ResourceHandlerRegistry ) = {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
+  }
+
+  @Override
+  override def configureDefaultServletHandling( configurer : DefaultServletHandlerConfigurer ) {
+    configurer.enable()
   }
 }
 
